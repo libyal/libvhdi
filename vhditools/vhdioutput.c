2810a2ca9657b08f028160a67e7236922ba0a06b
Joachim Metz <joachim.metz@gmail.com>
2017-02-20 07:26:46 +0100

    Applied updates

--- vhditools/vhdioutput.c
+++ vhditools/vhdioutput.c
@@ -1,7 +1,7 @@
 /*
  * Common output functions for the vhditools
  *
- * Copyright (C) 2012-2016, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2012-2017, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
@@ -46,7 +46,7 @@ void vhdioutput_copyright_fprint(
 	 */
 	fprintf(
 	 stream,
-	 _( "Copyright (C) 2012-2016, %s.\n" ),
+	 _( "Copyright (C) 2012-2017, %s.\n" ),
 	 _( "Joachim Metz" ) );
 
 	fprintf(
