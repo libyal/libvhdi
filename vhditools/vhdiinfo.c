012bd8b942b40062578104dee98228429080ba44
Joachim Metz <joachim.metz@gmail.com>
2022-02-22 07:10:03 +0100

    Applied updates

--- vhditools/vhdiinfo.c
+++ vhditools/vhdiinfo.c
@@ -1,7 +1,7 @@
 /*
  * Shows information obtained from a Virtual Hard Disk (VHD) image file
  *
- * Copyright (C) 2012-2021, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2012-2022, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
