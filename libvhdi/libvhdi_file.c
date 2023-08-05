88e620cb624ddfe0ea63ed9dc0568a6cc863a0e7
Joachim Metz <joachim.metz@gmail.com>
2023-08-05 07:40:31 +0200

    Applied updates

--- libvhdi/libvhdi_file.c
+++ libvhdi/libvhdi_file.c
@@ -635,11 +635,11 @@ int libvhdi_file_open_file_io_handle(
      int access_flags,
      libcerror_error_t **error )
 {
-	libvhdi_internal_file_t *internal_file = NULL;
-	static char *function                  = "libvhdi_file_open_file_io_handle";
-	int bfio_access_flags                  = 0;
-	int file_io_handle_is_open             = 0;
-	int file_io_handle_opened_in_library   = 0;
+	libvhdi_internal_file_t *internal_file   = NULL;
+	static char *function                    = "libvhdi_file_open_file_io_handle";
+	uint8_t file_io_handle_opened_in_library = 0;
+	int bfio_access_flags                    = 0;
+	int file_io_handle_is_open               = 0;
 
 	if( file == NULL )
 	{
