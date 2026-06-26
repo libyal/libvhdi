5b69ca7815857f4656ff529c80a506935fc54ec3
Joachim Metz <joachim.metz@gmail.com>
2026-06-26 17:36:41 +0200

    Worked on OSS-Fuzz targets

--- ossfuzz/file_fuzzer.cc
+++ ossfuzz/file_fuzzer.cc
@@ -47,8 +47,21 @@ int LLVMFuzzerTestOneInput(
      const uint8_t *data,
      size_t size )
 {
+	uint8_t buffer[ 512 ];
+	uint8_t guid[ 16 ];
+	uint8_t utf8_string[ 64 ];
+	uint16_t utf16_string[ 64 ];
+
 	libbfio_handle_t *file_io_handle = NULL;
 	libvhdi_file_t *file             = NULL;
+	off64_t media_offset             = 0;
+	size64_t media_size              = 0;
+	size_t string_size               = 0;
+	uint32_t value_32bit             = 0;
+	uint16_t major_version           = 0;
+	uint16_t minor_version           = 0;
+	int file_type                    = 0;
+	int read_iterator                = 0;
 
 	if( libbfio_memory_range_initialize(
 	     &file_io_handle,
@@ -77,6 +90,107 @@ int LLVMFuzzerTestOneInput(
 	     NULL ) != 1 )
 	{
 		goto on_error_libvhdi;
+	}
+	if( libvhdi_file_get_file_type(
+	     file,
+	     &file_type,
+	     NULL ) != 1 )
+	{
+		goto on_error_libvhdi;
+	}
+	if( libvhdi_file_get_format_version(
+	     file,
+	     &major_version,
+	     &minor_version,
+	     NULL ) != 1 )
+	{
+		goto on_error_libvhdi;
+	}
+	if( libvhdi_file_get_disk_type(
+	     file,
+	     &value_32bit,
+	     NULL ) != 1 )
+	{
+		goto on_error_libvhdi;
+	}
+	if( libvhdi_file_get_bytes_per_sector(
+	     file,
+	     &value_32bit,
+	     NULL ) != 1 )
+	{
+		goto on_error_libvhdi;
+	}
+	if( libvhdi_file_get_identifier(
+	     file,
+	     guid,
+	     16,
+	     NULL ) != 1 )
+	{
+		goto on_error_libvhdi;
+	}
+	if( libvhdi_file_get_parent_identifier(
+	     file,
+	     guid,
+	     16,
+	     NULL ) == -1 )
+	{
+		goto on_error_libvhdi;
+	}
+	if( libvhdi_file_get_utf8_parent_filename_size(
+	     file,
+	     &string_size,
+	     NULL ) == -1 )
+	{
+		goto on_error_libvhdi;
+	}
+	if( libvhdi_file_get_utf8_parent_filename(
+	     file,
+	     utf8_string,
+	     64,
+	     NULL ) == -1 )
+	{
+		goto on_error_libvhdi;
+	}
+	if( libvhdi_file_get_utf16_parent_filename_size(
+	     file,
+	     &string_size,
+	     NULL ) == -1 )
+	{
+		goto on_error_libvhdi;
+	}
+	if( libvhdi_file_get_utf16_parent_filename(
+	     file,
+	     utf16_string,
+	     64,
+	     NULL ) == -1 )
+	{
+		goto on_error_libvhdi;
+	}
+	if( libvhdi_file_get_media_size(
+	     file,
+	     &media_size,
+	     NULL ) != 1 )
+	{
+		goto on_error_libvhdi;
+	}
+	for( read_iterator = 0;
+	     read_iterator < 128;
+	     read_iterator++ )
+	{
+		if( media_offset >= media_size )
+		{
+			break;
+		}
+		if( libvhdi_file_read_buffer_at_offset(
+		     file,
+		     buffer,
+		     497,
+		     media_offset,
+		     NULL ) == -1 )
+		{
+			goto on_error_libvhdi;
+		}
+		media_offset += 497;
 	}
 	libvhdi_file_close(
 	 file,
