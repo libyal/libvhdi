1162da7a1624992734d5b786993e5ec22a56c047
Joachim Metz <joachim.metz@gmail.com>
2026-09-17 04:52:30 +0200

    Worked on tests and changes for CI tests

--- tests/pkgbuild.sh
+++ tests/pkgbuild.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # Script to build a MacOS pkg
 #
-# Version: 20241015
+# Version: 20260914
 
 set -e
 
-make install DESTDIR=${PWD}/osx-pkg
-mkdir -p ${PWD}/osx-pkg/usr/share/doc/libvhdi
-cp AUTHORS COPYING COPYING.LESSER NEWS README ${PWD}/osx-pkg/usr/share/doc/libvhdi
+make install DESTDIR="${PWD}/osx-pkg"
+mkdir -p "${PWD}/osx-pkg/usr/share/doc/libvhdi"
+cp AUTHORS COPYING COPYING.LESSER NEWS README "${PWD}/osx-pkg/usr/share/doc/libvhdi"
 
-VERSION=`sed '5!d; s/^  \[//;s/\],$//' configure.ac`
-pkgbuild --root osx-pkg --identifier com.github.libyal.libvhdi --version ${VERSION} --ownership recommended ../libvhdi-${VERSION}.pkg
+VERSION=$(sed '5!d; s/^  \[//;s/\],$//' configure.ac)
+pkgbuild --root osx-pkg --identifier com.github.libyal.libvhdi --version "${VERSION}" --ownership recommended "../libvhdi-${VERSION}.pkg"
 
--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libvhdi/.libs/libvhdi.1.dylib && test -f ./pyvhdi/.libs/pyvhdi.so
+if [ -f "${PWD}/libvhdi/.libs/libvhdi.1.dylib" ] && [ -f ./pyvhdi/.libs/pyvhdi.so ]
 then
-	install_name_tool -change /usr/local/lib/libvhdi.1.dylib ${PWD}/libvhdi/.libs/libvhdi.1.dylib ./pyvhdi/.libs/pyvhdi.so
+    install_name_tool -change /usr/local/lib/libvhdi.1.dylib "${PWD}/libvhdi/.libs/libvhdi.1.dylib" ./pyvhdi/.libs/pyvhdi.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
