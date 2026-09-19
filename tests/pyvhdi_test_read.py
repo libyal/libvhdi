f6416e99973e816cf3a6d37b9e025d6aff1dd57f
Joachim Metz <joachim.metz@gmail.com>
2026-09-19 09:23:24 +0200

    Worked on tests and changes for CI tests

--- tests/pyvhdi_test_read.py
+++ tests/pyvhdi_test_read.py
@@ -28,319 +28,327 @@ import pyvhdi
 
 
 def get_whence_string(whence):
-  """Retrieves a human readable string representation of the whence."""
-  if whence == os.SEEK_CUR:
-    whence_string = "SEEK_CUR"
-  elif whence == os.SEEK_END:
-    whence_string = "SEEK_END"
-  elif whence == os.SEEK_SET:
-    whence_string = "SEEK_SET"
-  else:
-    whence_string = "UNKNOWN"
-  return whence_string
+    """Retrieves a human readable string representation of the whence."""
+    if whence == os.SEEK_CUR:
+        whence_string = "SEEK_CUR"
+    elif whence == os.SEEK_END:
+        whence_string = "SEEK_END"
+    elif whence == os.SEEK_SET:
+        whence_string = "SEEK_SET"
+    else:
+        whence_string = "UNKNOWN"
+    return whence_string
 
 
 def pyvhdi_test_seek_offset_and_read_buffer(
-    vhdi_file, input_offset, input_whence, input_size,
-    expected_offset, expected_size):
-  """Tests seeking an offset and reading a buffer."""
-  description = (
-      "Testing reading buffer at offset: {0:d}, whence: {1:s} of size: {2:d}"
-      "\t").format(input_offset, get_whence_string(input_whence), input_size)
-  print(description, end="")
+    vhdi_file, input_offset, input_whence, input_size, expected_offset, expected_size
+):
+    """Tests seeking an offset and reading a buffer."""
+    description = (
+        "Testing reading buffer at offset: {0:d}, whence: {1:s} of size: {2:d}" "\t"
+    ).format(input_offset, get_whence_string(input_whence), input_size)
+    print(description, end="")
 
-  error_string = None
-  result = True
-  try:
-    vhdi_file.seek(input_offset, input_whence)
+    error_string = None
+    result = True
+    try:
+        vhdi_file.seek(input_offset, input_whence)
 
-    result_offset = vhdi_file.get_offset()
-    if expected_offset != result_offset:
-      result = False
+        result_offset = vhdi_file.get_offset()
+        if expected_offset != result_offset:
+            result = False
 
-    if result:
-      result_size = 0
-      while input_size > 0:
-        read_size = 4096
-        if input_size < read_size:
-          read_size = input_size
+        if result:
+            result_size = 0
+            while input_size > 0:
+                read_size = 4096
+                if input_size < read_size:
+                    read_size = input_size
 
-        data = vhdi_file.read(size=read_size)
-        data_size = len(data)
+                data = vhdi_file.read(size=read_size)
+                data_size = len(data)
 
-        input_size -= data_size
-        result_size += data_size
+                input_size -= data_size
+                result_size += data_size
 
-        if data_size != read_size:
-          break
+                if data_size != read_size:
+                    break
 
-      if result_size != expected_size:
-        error_string = "Unexpected read count: {0:d}".format(result_size)
-        result = False
+            if result_size != expected_size:
+                error_string = "Unexpected read count: {0:d}".format(result_size)
+                result = False
 
-  except Exception as exception:
-    error_string = str(exception)
-    if expected_offset != -1:
-      result = False
+    except Exception as exception:
+        error_string = str(exception)
+        if expected_offset != -1:
+            result = False
 
-  if not result:
-    print("(FAIL)")
-  else:
-    print("(PASS)")
+    if not result:
+        print("(FAIL)")
+    else:
+        print("(PASS)")
 
-  if error_string:
-    print(error_string)
-  return result
+    if error_string:
+        print(error_string)
+    return result
 
 
 def pyvhdi_test_read_buffer_at_offset(
-    vhdi_file, input_offset, input_size,
-    expected_offset, expected_size):
-  """Tests reading a buffer at a specific offset."""
-  description = (
-      "Testing reading buffer at offset: {0:d} and size: {1:d}"
-      "\t").format(input_offset, input_size)
-  print(description, end="")
+    vhdi_file, input_offset, input_size, expected_offset, expected_size
+):
+    """Tests reading a buffer at a specific offset."""
+    description = (
+        "Testing reading buffer at offset: {0:d} and size: {1:d}" "\t"
+    ).format(input_offset, input_size)
+    print(description, end="")
 
-  error_string = None
-  result = True
-  try:
-    result_size = 0
-    while input_size > 0:
-      read_size = 4096
-      if input_size < read_size:
-        read_size = input_size
+    error_string = None
+    result = True
+    try:
+        result_size = 0
+        while input_size > 0:
+            read_size = 4096
+            if input_size < read_size:
+                read_size = input_size
 
-      data = vhdi_file.read_buffer_at_offset(read_size, input_offset)
-      data_size = len(data)
+            data = vhdi_file.read_buffer_at_offset(read_size, input_offset)
+            data_size = len(data)
 
-      input_offset += data_size
-      input_size -= data_size
-      result_size += data_size
+            input_offset += data_size
+            input_size -= data_size
+            result_size += data_size
 
-      if data_size != read_size:
-        break
+            if data_size != read_size:
+                break
 
-    if input_offset != expected_offset:
-      error_string = "Unexpected offset: {0:d}".format(input_offset)
-      result = False
+        if input_offset != expected_offset:
+            error_string = "Unexpected offset: {0:d}".format(input_offset)
+            result = False
 
-    elif result_size != expected_size:
-      error_string = "Unexpected read count: {0:d}".format(result_size)
-      result = False
+        elif result_size != expected_size:
+            error_string = "Unexpected read count: {0:d}".format(result_size)
+            result = False
 
-  except Exception as exception:
-    error_string = str(exception)
-    if expected_offset != -1:
-      result = False
+    except Exception as exception:
+        error_string = str(exception)
+        if expected_offset != -1:
+            result = False
 
-  if not result:
-    print("(FAIL)")
-  else:
-    print("(PASS)")
+    if not result:
+        print("(FAIL)")
+    else:
+        print("(PASS)")
 
-  if error_string:
-    print(error_string)
-  return result
+    if error_string:
+        print(error_string)
+    return result
 
 
 def pyvhdi_test_read(vhdi_file):
-  """Tests the read function."""
-  media_size = vhdi_file.media_size
+    """Tests the read function."""
+    media_size = vhdi_file.media_size
 
-  # Case 0: test full read
+    # Case 0: test full read
 
-  # Test: offset: 0 size: <media_size>
-  # Expected result: offset: 0 size: <media_size>
-  read_offset = 0
-  read_size = media_size
-
-  if not pyvhdi_test_seek_offset_and_read_buffer(
-      vhdi_file, read_offset, os.SEEK_SET, read_size,
-      read_offset, read_size):
-    return False
-
-  if not pyvhdi_test_seek_offset_and_read_buffer(
-      vhdi_file, read_offset, os.SEEK_SET, read_size,
-      read_offset, read_size):
-    return False
-
-  # Case 1: test buffer at offset read
-
-  # Test: offset: <media_size / 7> size: <media_size / 2>
-  # Expected result: offset: <media_size / 7> size: <media_size / 2>
-  read_offset, _ = divmod(media_size, 7)
-  read_size, _ = divmod(media_size, 2)
-
-  if not pyvhdi_test_seek_offset_and_read_buffer(
-      vhdi_file, read_offset, os.SEEK_SET, read_size,
-      read_offset, read_size):
-    return False
-
-  if not pyvhdi_test_seek_offset_and_read_buffer(
-      vhdi_file, read_offset, os.SEEK_SET, read_size,
-      read_offset, read_size):
-    return False
-
-  # Case 2: test read beyond media size
-
-  if media_size < 1024:
-    # Test: offset: <media_size - 1024> size: 4096
-    # Expected result: offset: -1 size: <undetermined>
-    read_offset = media_size - 1024
-    read_size = 4096
+    # Test: offset: 0 size: <media_size>
+    # Expected result: offset: 0 size: <media_size>
+    read_offset = 0
+    read_size = media_size
 
     if not pyvhdi_test_seek_offset_and_read_buffer(
-        vhdi_file, read_offset, os.SEEK_SET, read_size, -1, -1):
-      return False
+        vhdi_file, read_offset, os.SEEK_SET, read_size, read_offset, read_size
+    ):
+        return False
 
     if not pyvhdi_test_seek_offset_and_read_buffer(
-        vhdi_file, read_offset, os.SEEK_SET, read_size, -1, -1):
-      return False
+        vhdi_file, read_offset, os.SEEK_SET, read_size, read_offset, read_size
+    ):
+        return False
 
-  else:
-    # Test: offset: <media_size - 1024> size: 4096
-    # Expected result: offset: <media_size - 1024> size: 1024
-    read_offset = media_size - 1024
-    read_size = 4096
+    # Case 1: test buffer at offset read
 
-    if not pyvhdi_test_seek_offset_and_read_buffer(
-        vhdi_file, read_offset, os.SEEK_SET, read_size,
-        read_offset, 1024):
-      return False
+    # Test: offset: <media_size / 7> size: <media_size / 2>
+    # Expected result: offset: <media_size / 7> size: <media_size / 2>
+    read_offset, _ = divmod(media_size, 7)
+    read_size, _ = divmod(media_size, 2)
 
     if not pyvhdi_test_seek_offset_and_read_buffer(
-        vhdi_file, read_offset, os.SEEK_SET, read_size,
-        read_offset, 1024):
-      return False
+        vhdi_file, read_offset, os.SEEK_SET, read_size, read_offset, read_size
+    ):
+        return False
 
-  # Case 3: test buffer at offset read
+    if not pyvhdi_test_seek_offset_and_read_buffer(
+        vhdi_file, read_offset, os.SEEK_SET, read_size, read_offset, read_size
+    ):
+        return False
 
-  # Test: offset: <media_size / 7> size: <media_size / 2>
-  # Expected result: offset: < ( media_size / 7 ) + ( media_size / 2 ) > size: <media_size / 2>
-  read_offset, _ = divmod(media_size, 7)
-  read_size, _ = divmod(media_size, 2)
+    # Case 2: test read beyond media size
 
-  if not pyvhdi_test_read_buffer_at_offset(
-      vhdi_file, read_offset, read_size,
-      read_offset + read_size, read_size):
-    return False
+    if media_size < 1024:
+        # Test: offset: <media_size - 1024> size: 4096
+        # Expected result: offset: -1 size: <undetermined>
+        read_offset = media_size - 1024
+        read_size = 4096
 
-  if not pyvhdi_test_read_buffer_at_offset(
-      vhdi_file, read_offset, read_size,
-      read_offset + read_size, read_size):
-    return False
+        if not pyvhdi_test_seek_offset_and_read_buffer(
+            vhdi_file, read_offset, os.SEEK_SET, read_size, -1, -1
+        ):
+            return False
 
-  return True
+        if not pyvhdi_test_seek_offset_and_read_buffer(
+            vhdi_file, read_offset, os.SEEK_SET, read_size, -1, -1
+        ):
+            return False
+
+    else:
+        # Test: offset: <media_size - 1024> size: 4096
+        # Expected result: offset: <media_size - 1024> size: 1024
+        read_offset = media_size - 1024
+        read_size = 4096
+
+        if not pyvhdi_test_seek_offset_and_read_buffer(
+            vhdi_file, read_offset, os.SEEK_SET, read_size, read_offset, 1024
+        ):
+            return False
+
+        if not pyvhdi_test_seek_offset_and_read_buffer(
+            vhdi_file, read_offset, os.SEEK_SET, read_size, read_offset, 1024
+        ):
+            return False
+
+    # Case 3: test buffer at offset read
+
+    # Test: offset: <media_size / 7> size: <media_size / 2>
+    # Expected result: offset: < ( media_size / 7 ) + ( media_size / 2 ) > size: <media_size / 2>
+    read_offset, _ = divmod(media_size, 7)
+    read_size, _ = divmod(media_size, 2)
+
+    if not pyvhdi_test_read_buffer_at_offset(
+        vhdi_file, read_offset, read_size, read_offset + read_size, read_size
+    ):
+        return False
+
+    if not pyvhdi_test_read_buffer_at_offset(
+        vhdi_file, read_offset, read_size, read_offset + read_size, read_size
+    ):
+        return False
+
+    return True
 
 
 def pyvhdi_test_read_file(filename):
-  """Tests the read function with a file."""
-  vhdi_file = pyvhdi.file()
+    """Tests the read function with a file."""
+    vhdi_file = pyvhdi.file()
 
-  vhdi_file.open(filename, "r")
+    vhdi_file.open(filename, "r")
 
-  vhdi_parent_file = None
-  if vhdi_file.parent_identifier:
-    vhdi_parent_file = pyvhdi.file()
+    vhdi_parent_file = None
+    if vhdi_file.parent_identifier:
+        vhdi_parent_file = pyvhdi.file()
 
-    parent_filename = os.path.join(
-      os.path.dirname(filename), vhdi_file.parent_filename)
-    vhdi_parent_file.open(parent_filename, "r")
+        parent_filename = os.path.join(
+            os.path.dirname(filename), vhdi_file.parent_filename
+        )
+        vhdi_parent_file.open(parent_filename, "r")
 
-    vhdi_file.set_parent(vhdi_parent_file)
+        vhdi_file.set_parent(vhdi_parent_file)
 
-  result = pyvhdi_test_read(vhdi_file)
-  vhdi_file.close()
+    result = pyvhdi_test_read(vhdi_file)
+    vhdi_file.close()
 
-  if vhdi_parent_file:
-    vhdi_parent_file.close()
+    if vhdi_parent_file:
+        vhdi_parent_file.close()
 
-  return result
+    return result
 
 
 def pyvhdi_test_read_file_object(filename):
-  """Tests the read function with a file-like object."""
-  file_object = open(filename, "rb")
-  vhdi_file = pyvhdi.file()
+    """Tests the read function with a file-like object."""
+    file_object = open(filename, "rb")
+    vhdi_file = pyvhdi.file()
 
-  vhdi_file.open_file_object(file_object, "r")
+    vhdi_file.open_file_object(file_object, "r")
 
-  vhdi_parent_file = None
-  if vhdi_file.parent_identifier:
-    vhdi_parent_file = pyvhdi.file()
+    vhdi_parent_file = None
+    if vhdi_file.parent_identifier:
+        vhdi_parent_file = pyvhdi.file()
 
-    parent_filename = os.path.join(
-      os.path.dirname(filename), vhdi_file.parent_filename)
-    vhdi_parent_file.open(parent_filename, "r")
+        parent_filename = os.path.join(
+            os.path.dirname(filename), vhdi_file.parent_filename
+        )
+        vhdi_parent_file.open(parent_filename, "r")
 
-    vhdi_file.set_parent(vhdi_parent_file)
+        vhdi_file.set_parent(vhdi_parent_file)
 
-  result = pyvhdi_test_read(vhdi_file)
-  vhdi_file.close()
+    result = pyvhdi_test_read(vhdi_file)
+    vhdi_file.close()
 
-  if vhdi_parent_file:
-    vhdi_parent_file.close()
+    if vhdi_parent_file:
+        vhdi_parent_file.close()
 
-  return result
+    return result
 
 
 def pyvhdi_test_read_file_no_open(filename):
-  """Tests the read function with a file without open."""
-  description = "Testing read of without open:\t"
-  print(description, end="")
+    """Tests the read function with a file without open."""
+    description = "Testing read of without open:\t"
+    print(description, end="")
 
-  vhdi_file = pyvhdi.file()
+    vhdi_file = pyvhdi.file()
 
-  error_string = None
-  result = False
-  try:
-    vhdi_file.read(size=4096)
-  except Exception as exception:
-    error_string = str(exception)
-    result = True
+    error_string = None
+    result = False
+    try:
+        vhdi_file.read(size=4096)
+    except Exception as exception:
+        error_string = str(exception)
+        result = True
 
-  if not result:
-    print("(FAIL)")
-  else:
-    print("(PASS)")
+    if not result:
+        print("(FAIL)")
+    else:
+        print("(PASS)")
 
-  if error_string:
-    print(error_string)
-  return result
+    if error_string:
+        print(error_string)
+    return result
 
 
 def main():
-  args_parser = argparse.ArgumentParser(
-      description="Tests read.")
+    args_parser = argparse.ArgumentParser(description="Tests read.")
 
-  args_parser.add_argument(
-      "source", nargs="?", action="store", metavar="FILENAME",
-      default=None, help="The source filename.")
+    args_parser.add_argument(
+        "source",
+        nargs="?",
+        action="store",
+        metavar="FILENAME",
+        default=None,
+        help="The source filename.",
+    )
 
-  options = args_parser.parse_args()
+    options = args_parser.parse_args()
 
-  if not options.source:
-    print("Source value is missing.")
-    print("")
-    args_parser.print_help()
-    print("")
-    return False
+    if not options.source:
+        print("Source value is missing.")
+        print("")
+        args_parser.print_help()
+        print("")
+        return False
 
-  if not pyvhdi_test_read_file(options.source):
-    return False
+    if not pyvhdi_test_read_file(options.source):
+        return False
 
-  if not pyvhdi_test_read_file_object(options.source):
-    return False
+    if not pyvhdi_test_read_file_object(options.source):
+        return False
 
-  if not pyvhdi_test_read_file_no_open(options.source):
-    return False
+    if not pyvhdi_test_read_file_no_open(options.source):
+        return False
 
-  return True
+    return True
 
 
 if __name__ == "__main__":
-  if not main():
-    sys.exit(1)
-  else:
-    sys.exit(0)
+    if not main():
+        sys.exit(1)
+    else:
+        sys.exit(0)
